765c89846bf105d4382ea60efa1722781c9bbeea
SaraAndrade1123 <valeandrade1123@gmail.com>
2026-09-01 06:06:18 -0500

    Usuario & contraseña requeridos!

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Aplicaciones propias
     'gestor_proyectos',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
